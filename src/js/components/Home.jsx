6ae54048b63bf40b7ac5b9112c3c04a60f7b902d
Damián Ramírez <146871059+damianconpan@users.noreply.github.com>
2025-02-05 23:56:13 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState }from "react";
+import React, { useState, useEffect }from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
@@ -8,6 +8,64 @@ const Home = () => {
 
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
+	useEffect(() =>{
+		getTodo()
+	}, [])
+
+	const getTodo = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/damian",
+				{
+					method: "GET",
+				},
+			)
+			const data = await response.json();
+		console.log(data);
+		setTodos(data.todos);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const deleteTodo = async (id) => {
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			)
+			const data = await response.json();
+		console.log(data);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+	const createUserTodo = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/todos/damian",
+			{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				label: inputValue,
+				is_done: false,
+			}),
+			} 
+		);
+		const data = await response.json();
+		console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 
 	return (
 		<div className="text-center">
@@ -20,7 +78,8 @@ const Home = () => {
 					onChange={(e) => setInputValue(e.target.value)}
 					value = {inputValue}
 					onKeyPress={(e) =>{
-						if (e.key === "Enter") {
+						if (e.key === "Enter" && inputValue.trim() !== "") {
+						createUserTodo(inputValue);
 						setTodos(todos.concat([inputValue]));
 						setInputValue("");	
 						}
@@ -31,11 +90,12 @@ const Home = () => {
 				</li>
 				{todos.map((item, index) =>(
 					<li className="listaDePendientes">
-						{item} <i className=" basurero fa-solid fa-trash" onClick={() =>setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+						{item.label} <i className=" basurero fa-solid fa-trash" onClick={() =>{setTodos(todos.filter((t, currentIndex) => index != currentIndex)), deleteTodo(item.id)}}></i>
 					</li>
 				))}
 			</ul>
 			<div className="xPendientes">{todos.length} pendientes</div>
+
 
 		</div>
 	);
